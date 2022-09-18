221becff9e137f5860a0570832f8d215cab7249f
Michael Ababio <michaelkwasi@gmail.com>
2022-09-18 05:14:43 -0400

    working on something

---
+++ tesla-get-location/main.py
@@ -0,0 +1,19 @@
+#import teslapy
+import json
+import teslapy
+
+def tesla_get_location(request):
+    with teslapy.Tesla('REMOVED') as tesla:
+        vehicles = tesla.vehicle_list()
+        vehicles[0].sync_wake_up()
+        tesla_data = vehicles[0].api('VEHICLE_DATA')['response']['drive_state']
+        lat = str(tesla_data['latitude'])
+        lon = str(tesla_data['longitude'])
+        tuple_latlon = (lat,lon)
+        data = {}
+        data['lat'] = lat
+        data['lon'] = lon
+        data['speed'] = tesla_data['speed']
+        json_data = json.dumps(data)
+    return json_data
+    #return str(tuple_latlon)
