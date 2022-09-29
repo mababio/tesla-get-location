d19eb7c896efa8e2a6b7a92f9a2bdd1d53efb21a
Michael Ababio <michaelkwasi@gmail.com>
2022-09-28 22:49:50 -0400

    bug fixing

--- src/main.py
+++ src/main.py
@@ -9,11 +9,32 @@ def tesla_get_location(request):
         tesla_data = vehicles[0].api('VEHICLE_DATA')['response']['drive_state']
         lat = str(tesla_data['latitude'])
         lon = str(tesla_data['longitude'])
-        tuple_latlon = (lat,lon)
-        data = {}
-        data['lat'] = lat
-        data['lon'] = lon
-        data['speed'] = tesla_data['speed']
+        data = {'lat': lat, 'lon': lon, 'speed': tesla_data['speed']}
         json_data = json.dumps(data)
     return json_data
-    #return str(tuple_latlon)
+
+
+
+#
+# import json
+# import teslapy
+# from logs import logger
+#
+#
+# def tesla_get_location(request):
+#     try:
+#         with teslapy.Tesla('REMOVED') as tesla:
+#             vehicles = tesla.vehicle_list()
+#             vehicles[0].sync_wake_up()
+#             tesla_data = vehicles[0].api('VEHICLE_DATA')['response']['drive_state']
+#             lat = str(tesla_data['latitude'])
+#             lon = str(tesla_data['longitude'])
+#             if lat and lon:
+#                 data = {'lat': lat, 'lon': lon, 'speed': tesla_data['speed']}
+#                 json_data = json.dumps(data)
+#             else:
+#                 raise ValueError("tesla_get_location::::: Either lat or lon is empty")
+#         return json_data
+#     except Exception as e:
+#         logger.error("tesla_get_location::::: Issue getting location::::: " + str(e))
+#         raise
