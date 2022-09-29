08b8efd2fc567f19a359447a39bf6a5321604873
Michael Ababio <michaelkwasi@gmail.com>
2022-09-28 22:21:27 -0400

    bug fixing

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from logs import logger
 
 
 def tesla_get_location(request):
-    try:
-        with teslapy.Tesla('REMOVED') as tesla:
-            vehicles = tesla.vehicle_list()
-            vehicles[0].sync_wake_up()
+    with teslapy.Tesla('REMOVED') as tesla:
+        vehicles = tesla.vehicle_list()
+        vehicles[0].sync_wake_up()
+        try:
             tesla_data = vehicles[0].api('VEHICLE_DATA')['response']['drive_state']
             lat = str(tesla_data['latitude'])
             lon = str(tesla_data['longitude'])
@@ -16,7 +16,7 @@ def tesla_get_location(request):
                 json_data = json.dumps(data)
             else:
                 raise ValueError("tesla_get_location::::: Either lat or lon is empty")
-        return json_data
-    except Exception as e:
-        logger.error("tesla_get_location::::: Issue getting location::::: " + str(e))
-        raise
+            return json_data
+        except Exception as e:
+            logger.error("tesla_get_location::::: Issue getting location::::: " + str(e))
+            raise
